7eda14f91995a07642f17feba0cbaa223727bff2
Ez-Link-QR <rohitsingh24092004@gmail.com>
2025-04-14 00:26:35 +0530

    Add files via upload

---
+++ faq.js
@@ -0,0 +1,28 @@
+function toggleAnswer(event) {
+            const answer = event.target.nextElementSibling;
+            if (answer.style.display === "none" || answer.style.display === "") {
+                answer.style.display = "block";
+            } else {
+                answer.style.display = "none";
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+ 
+      
+
+
+
+
+
+
+
