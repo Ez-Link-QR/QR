124fc63a40a7c2f3392996d7d96ab6140024e907
Ez-Link-QR <rohitsingh24092004@gmail.com>
2025-04-15 19:20:36 +0530

    Add files via upload

---
+++ create.js
@@ -0,0 +1,45 @@
+const inp = document.querySelector("#qrinput");
+const genbtn = document.querySelector("#generatebtn");
+const qrpopup = document.querySelector("#qrpopup");
+const qrimg = document.querySelector("#qrimg");
+const downloadbtn = document.querySelector("#downloadbtn");
+const closebtn = document.querySelector("#closebtn");   
+const maincontainer = document.querySelector("#maincontainer");
+
+const url = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=";
+
+genbtn.addEventListener("click", () => {
+   
+    if (!inp.value) {
+        alert("Enter url or text first!.");
+    }
+    else {
+        const imgurl=url + inp.value;
+        qrimg.setAttribute ('src' , imgurl);
+        setTimeout(() => {
+            qrpopup.classList.add("show");
+            maincontainer.classList.add("opacity");
+        },1000);
+
+    }
+}
+);
+
+
+downloadbtn.addEventListener("click", () => {
+    const imgurl = url + inp.value;
+    fetch(imgurl)
+        .then((response) => response.blob())
+        .then((blob) => {
+            const link = document.createElement('a');
+            const href = URL.createObjectURL(blob);
+           link.downloade = 'qr.jpg';
+           link.click();
+           
+        })
+    });
+      
+closebtn.addEventListener("click", () => {
+    qrpopup.classList.remove("show");
+    maincontainer.classList.remove("opacity");
+})    
--- faq.js
+++ faq.js
@@ -1,28 +1,17 @@
-function toggleAnswer(event) {
-            const answer = event.target.nextElementSibling;
-            if (answer.style.display === "none" || answer.style.display === "") {
-                answer.style.display = "block";
-            } else {
-                answer.style.display = "none";
+const accordin_item=document.querySelectorAll(".accordin-item")
+accordin_item.forEach(item=>{
+    const title=item.querySelector(".accordin-title")
+    const content=item.querySelector(".accordin-content")
+
+    title.addEventListener('click',()=>{
+        for(var i=0;i<accordin_item.length;i++){
+            if(accordin_item[i] != item){
+
+                accordin_item[i].classList.remove('active')
+            }
+            else{
+                item.classList.toggle("active")
             }
         }
-
-
-
-
-
-
-
-
-
-
-
- 
-      
-
-
-
-
-
-
-
+    })
+})
